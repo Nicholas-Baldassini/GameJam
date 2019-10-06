f87a5ba288856ec017b7c04d716a12b61464a262
Nicholas-Baldassini <46302707+Nicholas-Baldassini@users.noreply.github.com>
2019-10-06 14:15:49 -0400

    Update Hackathon.java

--- Hackathon/src/hackathon/Hackathon.java
+++ Hackathon/src/hackathon/Hackathon.java
@@ -1,21 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package hackathon;
 
-/**
- *
- * @author johnb
- */
-public class Hackathon {
+package _____;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+import javax.swing.*;
+import java.awt.event.*;  
+public class _____ {
+   
+
+public static void main(String[] args) { 
     
-}
+    JFrame Frame=new JFrame("Button Example");
+    
+    final JTextField TextBox=new JTextField();
+    
+    JButton Button=new JButton("Click Here");
+    
+    JLabel Label=new JLabel("Hello world");
+    
+    TextBox.setBounds(50,50, 150,20);  
+    
+    Label.setBounds(0,0,100,100);
+    
+    Button.setBounds(50,100,95,30);  
+    
+    Button.addActionListener(new ActionListener(){  
+    
+public void actionPerformed(ActionEvent e){  
+            
+            
+            TextBox.setText("hello world");  
+            
+        }  
+
+    });  
+    Frame.add(Button);
+    Frame.add(TextBox);
+    Frame.add(Label);
+    Frame.setSize(400,400);  
+    
+    Frame.setLayout(null);  
+
+    Frame.setVisible(true);   
+}  
+}  
+
